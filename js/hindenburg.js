12897220861ab9616ad2ac4db1a886f8c0f4564e
dascheaten <mbrennster@gmail.com>
2015-08-03 09:19:26 -0400

    Modals
    Got them modals working

--- js/hindenburg.js
+++ js/hindenburg.js
@@ -1,33 +1,36 @@
 // hide the modals and the background on load
-$(document).load('event', function() {
-	$('.modal_bg').hide();
-	$('.modal').hide();
-});
-
-// Show me modal windows
-$('.modal-go').bind('click', function() {
-	var dis_modal = $(this).attr('data-modal-id')
-	var close_bg = $(this).attr('data-modal-closebg')
-	$('.modal').hide();
-	$('.modal_bg').fadeIn();
-	$('#' + dis_modal).fadeIn().addClass('modal_on');
-});
-
-// Close on button
-$('.close').bind('click', function() {
-	$('.modal_bg').fadeOut();
-	$('.modal').fadeOut();
-});
-
-// close on background click, sometimes
-$('.modal_bg').bind('click', function() {
-	if close_bg = 'true' {
-		$('.modal_bg').fadeOut();
-		$('.modal').fadeOut();
+$(document).ready(function() {
+	if ($('.modal-go').length) {
+		$('body').append('<a class="modal_bg"></a>');
+		$('.modal').hide();
+		$('.modal_bg').hide();
 	}
+	
+	// Show me modal windows
+	$('.modal-go').bind('click', function() {
+		var dis_modal = $(this).attr('data-modal-id')
+		$('.modal').hide().removeClass('modal_on');
+		$('.modal_bg').fadeIn();
+		$('#' + dis_modal).fadeIn().addClass('modal_on');
+	});
+
+	// Close on button
+	$('.close').bind('click', function() {
+		$('.modal_bg').fadeOut();
+		$('.modal').fadeOut().removeClass('modal_on');
+	});
+
+	// close on background click, sometimes
+	$('.modal_bg').bind('click', function() {
+		var close_bg = $('.modal_on').attr('data-modal-closebg')
+		if (close_bg != 'false') {
+			$('.modal_bg').fadeOut();
+			$('.modal').fadeOut();
+		}
+	});
+
+	$('span.close').bind('click', function() {
+		$(this).parent().slideUp();
+	});
 });
 
-
-$('span.close').bind('click', function() {
-	$(this).parent().slideUp();
-});
