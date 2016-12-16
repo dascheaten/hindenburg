f393493242da8d39dba697074d6279de13578a5f
dascheaten <mbrennster@gmail.com>
2016-12-16 12:40:53 -0500

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	index.html
    #	js/hindenburg.js
    #	sass/_buttons.scss
    #	sass/_forms.scss
    #	sass/_grid.scss
    #	sass/_mixins.scss
    #	sass/_modals.scss
    #	sass/_settings.scss
    #	sass/_slider.scss
    #	sass/base.scss
    #	stylesheets/base.css

--- js/hindenburg.js
+++ js/hindenburg.js
@@ -1,13 +1,48 @@
+$(window).on('load', function() {
+	// Fix the forms if there's already data
+	$('input').each(function() {
+		if ($(this).val() !== '') {
+			var disput = $(this).attr('id');
+			var label = $("label[for='" + disput + "']");
+			$(label).addClass("complete");
+		} else if ($(this).attr('disabled')) {
+			var disput = $(this).attr('id');
+			var label = $("label[for='" + disput + "']");
+			$(label).addClass("disabled");
+		}
+		if ($(this).prop('type') == 'range') {
+			insaneSlider($(this));
+		}
+	});
+});
+
 // hide the modals and the background on load
+function closeModal() {
+	$('.modal-bg').fadeOut();
+	$('.modal').fadeOut().removeClass('modal-on');
+}
+
+function showModal(dis) {
+	$('.modal').hide().removeClass('modal-on');
+	$('.modal-bg').fadeIn();
+	$(dis).fadeIn().addClass('modal-on');
+}
+
+// off canvas
+if ($('.off-canvas').length) {
+	$('body').append('<div class="off-canvas-bg"></div>');
+	$('.off-canvas-bg').hide();
+}
+
 $(document).ready(function () {
 	if ($('.modal').length) {
 		$('body').append('<a class="modal-bg"></a>');
-		$('.modal').hide();
+		$('.modal').not('[data-load]').hide();
 		$('.modal-bg').hide();
-	}
+	};
 	
 	// Show me modal windows
-	$('.modal-go').bind('click', function () {
+	$('[data-modal-id]').click(function() {
 		var dis_modal = $(this).attr('data-modal-id')
 		$('.modal').hide().removeClass('modal-on');
 		$('.modal-bg').fadeIn();
@@ -15,64 +50,61 @@ $(document).ready(function () {
 	});
 
 	// Close on button
-	$('.close').bind('click', function () {
-		$('.modal-bg').fadeOut();
-		$('.modal').fadeOut().removeClass('modal-on');
+	$('.close').click(function() {
+		closeModal();
 	});
+	
 
+	
 	// close on background click, sometimes
-	$('.modal-bg').bind('click', function () {
+	$('.modal-bg').click(function() {
 		var close_bg = $('.modal-on').attr('data-modal-closebg')
 		if (close_bg !== 'false') {
-			$('.modal-bg').fadeOut();
-			$('.modal').fadeOut();
+			closeModal();
 		}
 	});
 
-	$('span.close').bind('click', function () {
+	$('span.close').click(function() {
 		$(this).parent().slideUp();
 	});
 
-	$('input[type=range]').change();
+	$('span.tooltip').each(function() {
+		var tip = $(this).attr('title')
+		$(this).removeAttr('title').attr('data-tip', tip);
+	});
 	
+
+	$('select').each(function() {
+		if ($(this).val() !== '') {
+			var disput = $(this).attr('id');
+			var label = $("label[for='" + disput + "']");
+			$(label).addClass("complete");
+		} else if ($(this).attr('disabled')) {
+			var disput = $(this).attr('id');
+			var label = $("label[for='" + disput + "']");
+			$(label).addClass("disabled");
+		}
+	});
+
 });
 
-
-// Donut progress bar setter'er on load
-if ($('.progress-bar').hasClass('donut')) {
-    var percentage = $('.donut').attr('data-complete')
-    var donut_length = 440 - (percentage * 4.4)
-    var animation = '<div class="destroy-me"><style>@-webkit-keyframes donut { to { stroke-dashoffset: ' + donut_length + '; } } @keyframes donut { to { stroke-dashoffset: ' + donut_length + '; } }</style></div>'
-
-    $('.done').html(percentage);
-    $('.donut').prepend(animation).delay(1).show(1).attr('data-old-value', donut_length);
-}
-
-// call this function if you change the percentage value
-function changeValue() {
-    var old_value = $('.donut').attr('data-old-value');
-    var percentage = $('.donut').attr('data-complete')
-    var new_length = 440 - (percentage * 4.4)
-    var animation = '<div class="destroy-me"><style>.circle-animation{stroke-dashoffset: ' + old_value + ' !important;}@-webkit-keyframes donut { to { stroke-dashoffset: ' + new_length + '; } } @keyframes donut { to { stroke-dashoffset: ' + new_length + '; } }</style></div>'
-    var height = $('.donut').height()
-    
-    $('.destroy-me').remove();
-    $('.done').html(percentage);
-    $('.donut').css('height', height).attr('data-old-value', new_length);
-    $('.donut svg').hide(1).prepend(animation).delay(1).show(1);
-};
-
 // Set the progress on bar style progress bars
-function setProgress()  {
+function setProgress() {
     var percent = $('.progress-bar').attr('data-complete')
     $('.progress-bar.bar').children('.complete').animate({
-    width: percent + '%'
-  }, 1000 );  
+    		width: percent + '%'
+  	}, 1000 );  
 }
 
 if ($('.progress-bar').hasClass('bar')) {
   setProgress();
 }
+
+// Make Scrolling Great Again
+//$(".scroll").click(function(event){		
+//	event.preventDefault();
+//	$('html,body').animate({scrollTop:$(this.hash).offset().top -70}, 700);
+//});
 
 // Functions for Mega Menu
 if ($('nav').hasClass('mega-menu')) {
@@ -87,18 +119,24 @@ if ($('nav').hasClass('mega-menu')) {
 }
 
 // off canvas nav
-$('.off-canvas').bind('click', function () {
-	$(this).toggleClass('open');
-	if ($(this).hasClass('open')) {
-		$(this).text('Close');
-	}
-	else {
-		$(this).text('Menu');
-	}
+$('.off-canvas').on('click', function () {
+   $(this).toggleClass('open');
+   if ($(this).hasClass('open')) {
+	  $('.off-canvas-bg').fadeIn();
+   }
+   else {
+	  $('.off-canvas-bg').fadeOut();
+   }
 });
 
+$('.off-canvas-bg').on('click', function () {	
+	$('.off-canvas').toggleClass('open');
+	$('.off-canvas-bg').fadeOut();
+});
+
+
 // Functions for off canvans nav
-$('.drop').bind('click', function () {
+$('.drop').click(function() {
 	if ($('.off-canvas').hasClass('open')) {
 		$(this).toggleClass('drop-open');
 		if ($(this).hasClass('drop-open')) {
@@ -115,7 +153,7 @@ $('.drop').bind('click', function () {
 });	
 
 // Tool Tips
-$('span.tooltip').mouseenter(function () {
+$('.tooltip').on('click', function () {
 	var tip = $(this).attr('data-tip');
 	var content = $(this).html();
 	if (!$(this).hasClass('tip-open')){
@@ -125,18 +163,43 @@ $('span.tooltip').mouseenter(function () {
 		$('.this-tip').remove();
 	}
 });
-$('span.tooltip').mouseleave(function () {
+$('.tooltip').mouseleave(function () {
 	if ($(this).hasClass('tip-open')) {
 		$(this).removeClass('tip-open');
 		$('.this-tip').remove();
 	}
 });
 
+
+
 // Range Slider
-$('input[type=range]').on('change', function () {
-	var value = $(this).val();
-	$(this).prev().text(value);
+function insaneSlider(thisObj) {
+	// Seriously tho, this is fucking nuts
+		var value = thisObj.val(),
+		      min = thisObj.prop('min'),
+		      max = thisObj.prop('max'),
+		     step = thisObj.prop('step'),
+ 	 num_positions = (max - min) / step,
+	 	 position = (max - value) / step,
+    	    	    ratio = position / num_positions,
+    	    percentage = ratio * 100;
+//		console.log(percentage);
+	
+	thisObj.css('background-position', percentage + '% center')
+	
+//	if (thisObj.hasClass('percent')) {
+//		thisObj.prev().text(value + "%");
+//	} else {
+//		thisObj.prev().text(value);
+//	}
+}
+
+$('input[type=range]').on('input', function () {
+	insaneSlider( $(this) );
 });
+
+
+
 
 // Accordion make do'er
 $('dl.accordion dt').on('click', function () {
@@ -153,78 +216,181 @@ $('dl.accordion dt').on('click', function () {
 	}
 });
 
-// Make the labels do junk
-$('input').focus(function(){
-    $(this).prev('label').addClass('focus');
-});
 
-$('input').focusout(function(){
-    if( !$(this).val() ) {
-        $(this).prev('label').removeClass('focus');
-    }
-});
-
-// Slider
-$('#checkbox').change(function (){
-setInterval(function () {
-    moveRight();
-}, 3000);
-});
-
-jQuery(document).ready(function ($) {
-	var slideCount = $('#slider ul li').length;
- 	var slideWidth = '100%';
-	var slideHeight = $('#slider ul li.active').height();
-	var sliderUlWidth = '100%';
-
-	$('#slider').css({ width: '100%', height: slideHeight });
-	$('#slider ul').css({ width: sliderUlWidth, marginLeft: - slideWidth });
-
-	$('#slider ul li:first-child').prependTo('#slider ul');
-
-	function moveRight() {
-		$('.active').removeClass('active');
-        $('#slider ul li:first-child').appendTo('#slider ul');
-		$('#slider ul li:first-child').addClass('active');
-        var slideHeight = $('#slider ul li.active').height();
-		$('#slider ul').css('left', '');
-		$('#slider').css({ width: '100%', height: slideHeight });
-		$('#slider ul').css({ width: sliderUlWidth, marginLeft: - slideWidth });
-	};
-
-	function moveLeft() {
-		$('.active').removeClass('active');
-        $('#slider ul li:last-child').prependTo('#slider ul');
-		$('#slider ul li:first-child').addClass('active');
-		var slideHeight = $('#slider ul li.active').height();
-        $('#slider ul').css('left', '');
-		$('#slider').css({ width: '100%', height: slideHeight });
-		$('#slider ul').css({ width: sliderUlWidth, marginLeft: - slideWidth });
-	};
-
-	$('a.control_next').click(function () {
-	    moveRight();
+// Additional Questions
+var slide_number = 1;
+var previous_slide = 0;
+function setSlides() {
+	$('.additional-questions li').each(function() {
+		$(this).find('a.previous-question').attr('data-slide', previous_slide);
+		previous_slide++;	
+		$(this).attr('data-slide', slide_number);
+		slide_number++;
+		$(this).find('.next-question').attr('data-slide', slide_number);
 	});
+}
 
-	$('a.control_prev').click(function () {
-	    moveLeft();
-	});
-});
+function nextSlide(disThing) {
+	var slide = $(disThing).attr('data-slide');
+	var ul_height = $('.additional-questions li[data-slide="' + slide + '"]').outerHeight();
 
-$( window ).resize(function () {
-	var slideHeight = $('#slider ul li.active').height();
-	var sliderUlWidth = '100%';
+	$('.additional-questions ul').animate({
+		'height': ul_height + 'px'
+	}, 1000 );
 
-	$('#slider').css({ width: '100%', height: slideHeight });
-});
+	$('.additional-questions li.active').animate({
+		'margin-left': '-100%'
+	}, 1000 ).removeClass('active').fadeOut();
+
+	$('.additional-questions li[data-slide="' + slide + '"]').show().css('margin-left', '100%').animate({
+		'margin-left': '0%'
+	}, 1000 ).addClass('active');
+	$('.current-question').text(slide);
+};
+
+function previousSlide(disThing) {
+	var slide = $(disThing).attr('data-slide');
+	var ul_height = $('.additional-questions li[data-slide="' + slide + '"]').outerHeight();
+	
+	$('.additional-questions ul').animate({
+    		'height': ul_height + 'px'
+  		}, 1000 );
+	
+	$('.additional-questions li.active').animate({
+    		'margin-left': '100%'
+  		}, 1000 ).removeClass('active').fadeOut();
+	
+	$('.additional-questions li[data-slide="' + slide + '"]').show().css('margin-left', '-100%').animate({
+    		'margin-left': '0%'
+  		}, 1000 ).addClass('active');
+	
+	$('.current-question').text(slide);
+};
+
+if ($('.additional-questions').length > 0) {
+	var slide_count = $('.additional-questions ul li').length;
+	setSlides();
+	$('.additional-questions li[data-slide="1"]').addClass('active').show();
+//	$('.additional-questions .next-question').addClass('disabled');	
+	$('.question-number').text(slide_count);
+}
 
 // Tabs
 $('ul.tabs li').click(function(){
-    var tab_id = $(this).attr('data-tab');
-
-    $('ul.tabs li').removeClass('current');
-    $('.tab-content').removeClass('current');
-
-    $(this).addClass('current');
-    $("#"+tab_id).addClass('current');
+	var tab_id = $(this).attr('data-tab');
+	var current = $(this).parent().parent().find('ul.tabs li.current').attr('data-tab');
+	
+	$('.tab-link[data-tab="' + current + '"]').removeClass('current');
+	$("#" + current).removeClass('current');
+	
+	$(this).addClass('current');
+	$("#" + tab_id).addClass('current');
 });
+
+// Forms
+$("input, select").focus(function() {
+	var disput = $(this).attr('id');
+	var label = $("label[for='" + disput + "']");
+	$(label).removeClass("complete").addClass("focused");
+});
+
+$("input, select").blur(function() {
+	var disput = $(this).attr('id');
+	var label = $("label[for='" + disput + "']");
+	$(label).removeClass("focused");
+	if ($(this).val().length === 0) {
+		$(label).removeClass("focused complete");
+	} else if ($(this).hasClass('error')) {
+		$(label).addClass("error");
+	} else {
+		$(label).removeClass("focused error").addClass("complete");
+		$(disput).addClass("complete");
+	}
+});
+
+
+// because chrome and likely ie are little babies, we have to do this BS
+$("input, select").on('input', function() {
+	if (!$(this).is(":focus")) {
+		var disput = $(this).attr('id');
+		var label = $("label[for='" + disput + "']");
+		$(label).removeClass("focused");
+		if ($(this).val().length === 0) {
+			$(label).removeClass("focused complete");
+		} else if ($(this).hasClass('error')) {
+			$(label).addClass("error");
+		} else {
+			$(label).removeClass("focused error").addClass("complete");
+			$(disput).addClass("complete");
+		}
+	}
+});
+
+// Toggle button
+$('.toggle').click(function() {
+	var field = $(this).attr('data-field');
+	if(!$(this).hasClass('disabled')) {
+		$(this).toggleClass('on off');
+		if($(this).hasClass('off')) {
+			$('#' + field).val('off');
+			$('.' + field).text('Off');
+			$('[data-toggle-content="' + field + '"]').slideUp();
+			localStorage.setItem(field, 'off');
+		} else {
+			$('#' + field).val('on');
+			$('.' + field).text('On');
+			$('[data-toggle-content="' + field + '"]').slideDown();
+			localStorage.setItem(field, 'on');
+		}
+	}
+});
+
+
+// Count To function for the progress precentage
+// Have the percent complete count up all completely awesome like
+(function($) {
+    $.fn.countTo = function(options) {
+        // merge the default plugin settings with the custom options
+        options = $.extend({}, $.fn.countTo.defaults, options || {});
+
+        // how many times to update the value, and how much to increment the value on each update
+        var loops = Math.ceil(options.speed / options.refreshInterval),
+            increment = (options.to - options.from) / loops;
+
+        return $(this).each(function() {
+            var _this = this,
+                loopCount = 0,
+                value = options.from,
+                interval = setInterval(updateTimer, options.refreshInterval);
+
+            function updateTimer() {
+                value += increment;
+                loopCount++;
+                $(_this).html(value.toFixed(options.decimals));
+
+                if (typeof(options.onUpdate) == 'function') {
+                    options.onUpdate.call(_this, value);
+                }
+
+                if (loopCount >= loops) {
+                    clearInterval(interval);
+                    value = options.to;
+
+                    if (typeof(options.onComplete) == 'function') {
+                        options.onComplete.call(_this, value);
+                    }
+                }
+            }
+        });
+    };
+
+    $.fn.countTo.defaults = {
+        from: 0,  // the number the element should start at
+        to: 100,  // the number the element should end at
+        speed: 1000,  // how long it should take to count between the target numbers
+        refreshInterval: 100,  // how often the element should be updated
+        decimals: 0,  // the number of decimal places to show
+        onUpdate: null,  // callback method for every time the element is updated,
+        onComplete: null,  // callback method for when the element finishes updating
+    };
+})(jQuery);
